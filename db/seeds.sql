40413668c2eca750af797755c2cc115ceb542318
rodriguezh21 <rodriguezh21@gmail.com>
2020-04-30 22:32:02 -0500

    Added libraries and primary files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1, 'Cheeseburger', true);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, 'Bacon Burger', true);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3, 'Veggie Burger', true);
