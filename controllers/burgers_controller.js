fb8eee5d2f614d7478e54b9a8220d3ffc68b41ed
rodriguezh21 <rodriguezh21@gmail.com>
2020-04-30 22:36:16 -0500

    changed to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,7 @@ const express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
+
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
